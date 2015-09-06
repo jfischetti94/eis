624552bb5adab25657b484d01432a76d56f54822
jfischetti94 <tero.jnf@hotmail.com>
2015-09-06 02:23:11 -0300

    Fourth successful test

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -12,4 +12,31 @@ class Chopper
     return index
   end
   
+  def sum(anIntegerArray)
+    sum = 0
+    anIntegerArray.cycle(1) {|anInteger| sum += anInteger}
+    return self.arraySumDescription(sum)
+  end
+
+  def arraySumDescription(anInteger)
+    return self.oneDigitNumberDescriprion(anInteger)
+  end
+
+  def oneDigitNumberDescriprion(anInteger)
+    descriptions = {0 => "vacio",
+                    1 => "uno",
+                    2 => "dos",
+                    3 => "tres",
+                    4 => "cuatro",
+                    5 => "cinco",
+                    6 => "seis",
+                    7 => "siete",
+                    8 => "ocho",
+                    9 => "nueve"}
+    return descriptions.delete(anInteger)
+  end
+=begin
+  public    :chop, :sum
+  private   :arraySumDescription
+=end
 end
