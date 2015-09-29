9062787bf0255cc3d2aaae0fcd52a18a6d493315
jfischetti94 <tero.jnf@hotmail.com>
2015-09-29 20:11:57 -0300

    paper ties paper

--- piedra-papel-tijera-mono/model/paper.rb
+++ piedra-papel-tijera-mono/model/paper.rb
@@ -1,5 +1,9 @@
 class Paper
 
+  def play_with(some_move)
+    return Ties.new
+  end
+
   def play_with_rock()
     return Win.new
   end
@@ -7,4 +11,5 @@ class Paper
   def play_with_monkey()
     return Lose.new
   end
+
 end
---
+++ piedra-papel-tijera-mono/spec/paper_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec' 
+
+require_relative '../model/move'
+require_relative '../model/rock'
+require_relative '../model/paper'
+require_relative '../model/scissors'
+require_relative '../model/monkey'
+
+require_relative '../model/win'
+require_relative '../model/lose'
+require_relative '../model/ties'
+
+describe 'Paper' do
+
+  let(:rock) { Rock.new }
+  let(:paper) { Paper.new }
+  let(:scissors) { Scissors.new }
+  let(:monkey) { Monkey.new }
+  let(:win) { Win.new }
+  let(:lose) { Lose.new }
+  let(:ties) { Ties.new }
+
+  it 'paper ties paper' do
+    expect(paper.play_with(paper)).to eq ties
+  end
+
+end
