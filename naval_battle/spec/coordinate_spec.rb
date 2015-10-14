2692b69dca68ec5cb4b80669b7e51782faa4468a
jfischetti94 <tero.jnf@hotmail.com>
2015-10-14 13:12:29 -0300

    a coordinate (2,2) is x=2 y=2

--- naval_battle/spec/coordinate_spec.rb
+++ naval_battle/spec/coordinate_spec.rb
@@ -9,4 +9,10 @@ describe 'Coordinate' do
     expect( coord.y ).to eq 1
   end
 
+  it 'a coordinate (2,2) is x=2 y=2' do
+    coord = Coordinate.new(2,2)
+    expect( coord.x ).to eq 2
+    expect( coord.y ).to eq 2
+  end
+
 end
