fc86d6cfa682e71d33d8c2f112b8d517d2142556
jfischetti94 <tero.jnf@hotmail.com>
2015-10-17 01:30:04 -0300

    coord (1,8) its equals to coord (1,8)

--- battleship/spec/coord_spec.rb
+++ battleship/spec/coord_spec.rb
@@ -10,4 +10,10 @@ describe 'Coord' do
     expect( coord.y ).to eq 5
   end
 
+  it 'a coord (1,8) its equals to coord (1,8)' do
+    coord1 = Coord.new(1,8)
+    coord2 = Coord.new(1,8)
+    expect( coord1 ).to eq coord2
+  end
+
 end
