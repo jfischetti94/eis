c470e09c2316e43120b8988ffeffb1aa29ec4269
jfischetti94 <tero.jnf@hotmail.com>
2015-10-17 01:27:47 -0300

    Horizontal - obtains coords for 3 size

--- battleship/spec/orientation/horizontal_spec.rb
+++ battleship/spec/orientation/horizontal_spec.rb
@@ -9,7 +9,7 @@ describe 'Horizontal' do
   let( :horizontal ) {Horizontal.new()}
   let( :coord_1_1 ) {Coord.new(1,1)}
   let( :coord_2_1 ) {Coord.new(2,1)}
-  let( :coord_3_1 ) {Coord.new(2,1)}
+  let( :coord_3_1 ) {Coord.new(3,1)}
 
   it 'obtains coords for 1 size' do
     expect( horizontal.coords_from_for(coord_1_1,1) ).to eq [coord_1_1]
@@ -18,9 +18,9 @@ describe 'Horizontal' do
   it 'obtains coords for 2 size' do
     expect( horizontal.coords_from_for(coord_1_1,2) ).to eq [coord_1_1,coord_2_1]
   end
-=begin
+
   it 'obtains coords for 3 size' do
     expect( horizontal.coords_from_for(coord_1_1,3) ).to eq [coord_1_1, coord_2_1, coord_3_1]
   end
-=end
+
 end
