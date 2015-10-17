7fea4c40a5f76fd42a3d12137b5dc00b86896041
jfischetti94 <tero.jnf@hotmail.com>
2015-10-17 00:23:26 -0300

    put ship with long 3 verticaly

--- battleship/spec/board_spec.rb
+++ battleship/spec/board_spec.rb
@@ -10,6 +10,7 @@ describe 'Board' do
     @coord2_1 = instance_double("Coord", :x => 2, :y => 1)
     @coord3_1 = instance_double("Coord", :x => 3, :y => 1)
     @coord1_2 = instance_double("Coord", :x => 1, :y => 2)
+    @coord1_3 = instance_double("Coord", :x => 1, :y => 3)
     @coord = instance_double("Coord", :x => 1, :y => 1)
     @vertical = "vertical"
   end
@@ -69,6 +70,18 @@ describe 'Board' do
     board.put_ship(ship)
     expect( board.status_in(@coord1_1) ).to eq "taken"
     expect( board.status_in(@coord1_2) ).to eq "taken"
+  end
+
+  it 'put ship with long 3 horizontally' do
+    ship =  instance_double("Ship", :long => 1, :coords => [@coord1_1,@coord1_2,@coord1_3])
+
+    expect( board.status_in(@coord1_1) ).to eq "empty"
+    expect( board.status_in(@coord1_2) ).to eq "empty"
+    expect( board.status_in(@coord1_3) ).to eq "empty"
+    board.put_ship(ship)
+    expect( board.status_in(@coord1_1) ).to eq "taken"
+    expect( board.status_in(@coord1_2) ).to eq "taken"
+    expect( board.status_in(@coord1_3) ).to eq "taken"
   end
 
 end
