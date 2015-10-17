10605f3ee42377781d2bc475f6df35d62973f392
jfischetti94 <tero.jnf@hotmail.com>
2015-10-17 00:12:24 -0300

    put ship with long 2 horizontally

--- battleship/spec/board_spec.rb
+++ battleship/spec/board_spec.rb
@@ -6,7 +6,9 @@ describe 'Board' do
   let(:board) {Board.new(10,10)}
 
   before(:each) do
-    @origin_coord = instance_double("Coord", :x => 1, :y => 1)
+    @coord1_1 = instance_double("Coord", :x => 1, :y => 1)
+    @coord2_1 = instance_double("Coord", :x => 1, :y => 1)
+    @coord = instance_double("Coord", :x => 1, :y => 1)
     @vertical = "vertical"
   end
 
@@ -16,16 +18,25 @@ describe 'Board' do
   end
 
   it 'returned empty state when asked about the state of an empty cell' do
-    expect( board.status_in(@origin_coord) ).to eq "empty"    
+    expect( board.status_in(@coord1_1) ).to eq "empty"    
   end
 
   it 'put ship with long 1 horizontally' do
-    orientation = "vertical"
-    ship =  instance_double("Ship", :long => 1, :coords => [@origin_coord])
+    ship =  instance_double("Ship", :long => 1, :coords => [@coord1_1])
 
-    expect( board.status_in(@origin_coord) ).to eq "empty"
+    expect( board.status_in(@coord1_1) ).to eq "empty"
     board.put_ship(ship)
-    expect( board.status_in(@origin_coord) ).to eq "taken"
+    expect( board.status_in(@coord1_1) ).to eq "taken"
+  end
+
+  it 'put ship with long 2 horizontally' do
+    ship =  instance_double("Ship", :long => 1, :coords => [@coord1_1,@coord2_1])
+
+    expect( board.status_in(@coord1_1) ).to eq "empty"
+    expect( board.status_in(@coord2_1) ).to eq "empty"
+    board.put_ship(ship)
+    expect( board.status_in(@coord1_1) ).to eq "taken"
+    expect( board.status_in(@coord2_1) ).to eq "taken"
   end
 
 end
